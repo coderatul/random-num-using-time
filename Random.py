653dce286c8f46888bafac9d38b2cb31d3c868b6
Atul Kushwaha <atulkushwaha2008@gmail.com>
2023-06-16 16:39:47 +0530

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,24 +1,29 @@
-def randomnum():
-  #importing datetime module 
-  import datetime #inbuilt module
-  time_now  = datetime.datetime.now()
-  #getting time in terms of microseconds
-  random_seed = time_now.microsecond 
-   # an equation which returns a number between 0 and 10 & takes an integer as a parameter 
-  seed =  random_seed * 8 % 11
-  #making random number somewhat unpredictable !!!
-  random_micro_secs = []
-  for i in range(seed):
-    random_micro_secs.append(time_now.second**seed)
-  seed_2 = sum(random_micro_secs)
-  #generating final random number 
-  truely_random_num=(seed_2 * 8 % 11)
-  print(truely_random_num)
+import datetime
+import pytz  # Required library for time zone support
 
-  
+def randomnum(time_zone):
+    # Getting the current time in the specified time zone
+    time_now = datetime.datetime.now(pytz.timezone(time_zone))
+    
+    # Getting time in terms of microseconds
+    random_seed = time_now.microsecond
+    
+    # An equation which returns a number between 0 and 10
+    seed = random_seed * 8 % 11
+    
+    # Making random number somewhat unpredictable
+    random_micro_secs = []
+    for i in range(seed):
+        random_micro_secs.append(time_now.second**seed)
+    
+    seed_2 = sum(random_micro_secs)
+    
+    # Generating the final random number
+    truly_random_num = (seed_2 * 8 % 11)
+    
+    return truly_random_num
 
-  
-  
-  
-
-  
+# Example usage
+time_zone = 'America/New_York'  # Specify the desired time zone
+random_number = randomnum(time_zone)
+print(random_number)
